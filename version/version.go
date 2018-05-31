4b68b5d86efc1d3c597c85381fc699b712446e69
Li, Shulong <shulong.li@baishancloud.com>
2018-05-31 16:36:11 +0800

    serialize package

--- version/version.go
+++ version/version.go
@@ -1,3 +1,13 @@
 package version
 
-const VERSION = "1.0.0"
+const (
+	VERSION = "1.0.0"
+	/**
+	 * never change MAXLEN, it is long enough
+	 *
+	 * length of version string should be at least
+	 * one byte smaller than MAXLEN because having to make room
+	 * for delimter in byte array
+	 */
+	MAXLEN = 16
+)
