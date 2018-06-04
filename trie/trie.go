eff6e25a39dfbf1eb99be93359d6724d4ab2d97c
wenbo <wenbobuaa@foxmail.com>
2018-06-04 10:49:10 +0800

    trie.Search add modes: EQ|LT|GT
    TrieNode -> Node
    
    rm error KeyNotExist & return nil if not found
    
    trie receiver: root->r
    
    branch type byte->int
    
    define const leafBranch=-1
    
    refine function: neighborBranches
    
    refine function firstElt & lastElt
    
    refine mode: define basic mode E|T|G
    
    trie.Elt []byte -> trie.Value interface{}
    
    refine: firstValue()->leftMost() & lastValue()->rightMost()
    
    refine var name: E|L|G->EQ|LT|GT & Skip->Step

--- trie/trie.go
+++ trie/trie.go
@@ -1,91 +1,187 @@
 package trie
 
-import "errors"
-
-var KeyNotExist = errors.New("Key not Exist")
-
-type TrieNode struct {
-	Children map[byte]*TrieNode
-	Skip     uint16
-	IsLeaf   bool
-	Elt      []byte
+type Node struct {
+	Children map[int]*Node
+	Branches []int
+	Step     uint16
+	Value    interface{}
 }
 
-func New(key, value [][]byte) (root *TrieNode) {
+type Mode byte
 
-	root = &TrieNode{Children: make(map[byte]*TrieNode), Skip: 1}
+const (
+	LT Mode = 4 // less than    0100
+	EQ Mode = 2 // equal        0010
+	GT Mode = 1 // greater than 0001
+)
 
-	for i, k := range key {
+const leafBranch = -1
+
+func New(keys [][]byte, values []interface{}) (root *Node) {
+
+	root = &Node{Children: make(map[int]*Node), Step: 1}
+
+	for i, key := range keys {
 
 		var node = root
-		var n_pref int
+		var j int
+		var b byte
 
-		for j, b := range k {
-			n_pref = j
-			if node.Children[b] != nil {
-				node = node.Children[b]
-			} else {
+		for j, b = range key {
+			br := int(b)
+			if node.Children[br] == nil {
 				break
 			}
+			node = node.Children[br]
 		}
 
-		for _, b := range k[n_pref:] {
-			n := &TrieNode{Children: make(map[byte]*TrieNode), Skip: 1}
-			node.Children[b] = n
+		for _, b = range key[j:] {
+			br := int(b)
+			n := &Node{Children: make(map[int]*Node), Step: 1}
+
+			node.Children[br] = n
+			node.Branches = append(node.Branches, br)
 			node = n
 		}
 
-		node.IsLeaf = true
-		node.Elt = value[i]
-	}
+		leaf := &Node{Value: values[i]}
 
-	return root
+		node.Children[leafBranch] = leaf
+		node.Branches = append(node.Branches, leafBranch)
+	}
+	return
 }
 
-func (root *TrieNode) Squash() {
+func (r *Node) Squash() {
 
-	for k, n := range root.Children {
+	for k, n := range r.Children {
 		n.Squash()
 
-		if n.IsLeaf {
-			continue
-		}
-
-		if len(n.Children) == 1 {
-			for _, child := range n.Children {
-				child.Skip += 1
-				root.Children[k] = child
+		if len(n.Branches) == 1 {
+			if n.Branches[0] == leafBranch {
+				continue
 			}
+			child := n.Children[n.Branches[0]]
+			child.Step += 1
+			r.Children[k] = child
 		}
 	}
 }
 
-func (root *TrieNode) Search(key []byte) (value []byte, err error) {
+func (r *Node) Search(key []byte, mode Mode) (value interface{}) {
 
-	if root.IsLeaf {
-		value = root.Elt
+	var eqNode = r
+	var ltNode *Node
+	var gtNode *Node
+
+	for i := 0; ; {
+
+		var br int
+		if len(key) == i {
+			br = leafBranch
+		} else {
+			br = int(key[i])
+		}
+
+		li, ri := neighborBranches(eqNode.Branches, br)
+		if li >= 0 {
+			ltNode = eqNode.Children[eqNode.Branches[li]]
+		}
+		if ri >= 0 {
+			gtNode = eqNode.Children[eqNode.Branches[ri]]
+		}
+
+		eqNode = eqNode.Children[br]
+
+		if eqNode == nil {
+			break
+		}
+
+		if br == leafBranch {
+			break
+		}
+
+		i += int(eqNode.Step)
+
+		if i > len(key) {
+			gtNode = eqNode
+			eqNode = nil
+			break
+		}
+	}
+
+	if mode&LT == LT && ltNode != nil {
+		value = ltNode.rightMost().Value
+	}
+	if mode&GT == GT && gtNode != nil {
+		value = gtNode.leftMost().Value
+	}
+	if mode&EQ == EQ && eqNode != nil {
+		value = eqNode.Value
+	}
+
+	return
+}
+
+func neighborBranches(branches []int, br int) (ltIndex, rtIndex int) {
+
+	if len(branches) == 0 {
 		return
 	}
 
-	node := root
-	for i := 0; i < len(key); {
+	var i int
+	var b int
 
-		if node.Children[key[i]] == nil {
-			err = KeyNotExist
-			return
+	for i, b = range branches {
+		if b >= br {
+			break
 		}
-
-		node = node.Children[key[i]]
-		i += int(node.Skip)
-
-		if node.IsLeaf {
-			if i == len(key) {
-				value = node.Elt
-				return
-			}
-		}
-
 	}
-	err = KeyNotExist
+
+	if b == br {
+		rtIndex = i + 1
+		ltIndex = i - 1
+
+		if rtIndex == len(branches) {
+			rtIndex = -1
+		}
+		return
+	}
+
+	if b > br {
+		rtIndex = i
+		ltIndex = i - 1
+		return
+	}
+
+	rtIndex = -1
+	ltIndex = i
+
 	return
+}
+
+func (r *Node) leftMost() *Node {
+
+	node := r
+	for {
+		if len(node.Branches) == 0 {
+			return node
+		}
+
+		firstBr := node.Branches[0]
+		node = node.Children[firstBr]
+	}
+}
+
+func (r *Node) rightMost() *Node {
+
+	node := r
+	for {
+		if len(node.Branches) == 0 {
+			return node
+		}
+
+		lastBr := node.Branches[len(node.Branches)-1]
+		node = node.Children[lastBr]
+	}
 }
