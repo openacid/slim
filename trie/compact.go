7586dabb902913569908ee272e273815492aa320
shuwen5 <shuwen5@email.com>
2018-06-07 14:26:04 +0800

    add Compact Trie search

--- trie/compact.go
+++ trie/compact.go
@@ -2,21 +2,24 @@ package trie
 
 import (
 	"encoding/binary"
+	"unsafe"
+	"xec/bit"
 	"xec/sparse"
 )
 
-type children struct {
-	Bitmap uint16
-	Offset uint16
-}
+const WordMask = 0xf
+const MaxWord = 0x10
 
-type SparseTrie struct {
+type CompactTrie struct {
 	Children sparse.SparseArray
 	Steps    sparse.SparseArray
 	Leaves   sparse.SparseArray
 }
 
-const WordMask = 0xf
+type children struct {
+	Bitmap uint16
+	Offset uint16
+}
 
 type ChildConv struct {
 }
@@ -43,7 +46,7 @@ func (c ChildConv) GetMarshaledEltSize(b []byte) uint32 {
 	return uint32(4)
 }
 
-func (st *SparseTrie) FromSparse(root *Node) (err error) {
+func (st *CompactTrie) Compact(root *Node) (err error) {
 	if root == nil {
 		return
 	}
@@ -124,6 +127,158 @@ func (st *SparseTrie) FromSparse(root *Node) (err error) {
 	return nil
 }
 
-func (st *SparseTrie) Search(key []byte, mode Mode) (value interface{}) {
-	return nil
+func (st *CompactTrie) Search(key []byte, mode Mode) (value interface{}) {
+	eqIdx, ltIdx, gtIdx := int32(0), int32(-1), int32(-1)
+	ltLeaf := false
+
+	for idx := uint16(0); ; {
+		var word byte
+		if uint16(len(key)) == idx {
+			word = byte(MaxWord)
+		} else {
+			word = (uint8(key[idx]) & WordMask)
+		}
+
+		li, ei, ri, leaf := st.neighborBranches(uint16(eqIdx), word)
+		if li >= 0 {
+			ltIdx = li
+			ltLeaf = leaf
+		}
+
+		if ri >= 0 {
+			gtIdx = ri
+		}
+
+		eqIdx = ei
+		if eqIdx == -1 {
+			break
+		}
+
+		if word == MaxWord {
+			break
+		}
+
+		idx += st.getStep(uint16(eqIdx))
+
+		if idx > uint16(len(key)) {
+			gtIdx = eqIdx
+			eqIdx = -1
+			break
+		}
+	}
+
+	if mode&LT == LT && ltIdx != -1 {
+		if ltLeaf {
+			value = st.Leaves.Get(uint32(ltIdx))
+		} else {
+			rmIdx := st.rightMost(uint16(ltIdx))
+			value = st.Leaves.Get(uint32(rmIdx))
+		}
+	}
+	if mode&GT == GT && gtIdx != -1 {
+		fmIdx := st.leftMost(uint16(gtIdx))
+		value = st.Leaves.Get(uint32(fmIdx))
+	}
+	if mode&EQ == EQ && eqIdx != -1 {
+		value = st.Leaves.Get(uint32(eqIdx))
+	}
+
+	return
+}
+
+func (st *CompactTrie) getChild(idx uint16) *children {
+	cval := st.Children.Get(uint32(idx))
+	if cval == nil {
+		return nil
+	}
+	ch := cval.(children)
+
+	return &ch
+}
+
+func (st *CompactTrie) getStep(idx uint16) uint16 {
+	step := st.Steps.Get(uint32(idx))
+	if step == nil {
+		return uint16(1)
+	} else {
+		return step.(uint16)
+	}
+}
+
+func getChildIdx(ch *children, offset uint16) uint16 {
+	chNum := bit.Cnt1Before(uint64(ch.Bitmap), uint32(offset))
+	return ch.Offset + uint16(chNum) - uint16(1)
+}
+
+func (st *CompactTrie) neighborBranches(idx uint16, word byte) (ltIdx, eqIdx, rtIdx int32, ltLeaf bool) {
+	ltIdx, eqIdx, rtIdx = int32(-1), int32(-1), int32(-1)
+	ltLeaf = false
+
+	leaf := st.Leaves.Get(uint32(idx))
+
+	if word == MaxWord {
+		if leaf != nil {
+			eqIdx = int32(idx)
+		}
+	} else {
+		if leaf != nil {
+			ltIdx = int32(idx)
+			ltLeaf = true
+		}
+	}
+
+	ch := st.getChild(idx)
+	if ch == nil {
+		return
+	}
+
+	if (ch.Bitmap >> word & 1) == 1 {
+		eqIdx = int32(getChildIdx(ch, uint16(word+1)))
+	}
+
+	ltStart := uint8(word) & WordMask
+	for i := int8(ltStart) - 1; i >= 0; i-- {
+		if (ch.Bitmap >> uint8(i) & 1) == 1 {
+			ltIdx = int32(getChildIdx(ch, uint16(i+1)))
+			ltLeaf = false
+			break
+		}
+	}
+
+	rtStart := word + 1
+	if word == MaxWord {
+		rtStart = uint8(0)
+	}
+
+	for i := rtStart; i < MaxWord; i++ {
+		if (ch.Bitmap >> i & 1) == 1 {
+			rtIdx = int32(getChildIdx(ch, uint16(i+1)))
+			break
+		}
+	}
+
+	return
+}
+
+func (st *CompactTrie) leftMost(idx uint16) uint16 {
+	for {
+		if st.Leaves.Get(uint32(idx)) != nil {
+			return idx
+		}
+
+		ch := st.getChild(idx)
+		idx = ch.Offset
+	}
+}
+
+func (st *CompactTrie) rightMost(idx uint16) uint16 {
+	offset := uint16(unsafe.Sizeof(uint16(0)) * 8)
+	for {
+		ch := st.getChild(idx)
+		if ch == nil {
+			return idx
+		}
+
+		idx = getChildIdx(ch, offset)
+	}
 }
---
+++ trie/compact_test.go
@@ -0,0 +1,489 @@
+package trie
+
+import (
+	"reflect"
+	"testing"
+	"xec/sparse"
+)
+
+func wordKey(key []byte) []byte {
+	w := make([]byte, len(key)*2)
+
+	for i, k := range key {
+		w[i*2] = byte((k & 0xf0) >> 4)
+		w[i*2+1] = byte(k & 0x0f)
+	}
+
+	return w
+}
+
+func TestCompactTrie(t *testing.T) {
+
+	type ExpectType struct {
+		key  []byte
+		rst  uint16
+		mode Mode
+	}
+
+	var cases = []struct {
+		key      [][]byte
+		value    []interface{}
+		expected []ExpectType
+	}{
+		{
+			key: [][]byte{
+				{1, 2, 3},
+				{1, 2, 4},
+				{2, 3, 4},
+				{2, 3, 5},
+				{3, 4, 5},
+			},
+			value: []interface{}{
+				uint16(0),
+				uint16(1),
+				uint16(2),
+				uint16(3),
+				uint16(4),
+			},
+			expected: []ExpectType{
+				ExpectType{[]byte{1, 2, 3}, 0, EQ},
+				ExpectType{[]byte{1, 2, 4}, 1, EQ},
+				ExpectType{[]byte{2, 3, 4}, 2, EQ},
+				ExpectType{[]byte{2, 3, 5}, 3, EQ},
+				ExpectType{[]byte{3, 4, 5}, 4, EQ},
+			},
+		},
+		{
+			key: [][]byte{
+				{1, 2, 3},
+				{1, 2, 3, 4},
+				{2, 3},
+				{2, 3, 0},
+				{2, 3, 4},
+				{2, 3, 4, 5},
+				{2, 3, 15},
+			},
+			value: []interface{}{
+				uint16(0),
+				uint16(1),
+				uint16(2),
+				uint16(3),
+				uint16(4),
+				uint16(5),
+				uint16(6),
+			},
+			expected: []ExpectType{
+				ExpectType{[]byte{1, 2, 3}, 0, EQ},
+				ExpectType{[]byte{1, 2, 3, 4}, 1, EQ},
+				ExpectType{[]byte{2, 3}, 2, EQ},
+				ExpectType{[]byte{2, 3, 0}, 3, EQ},
+				ExpectType{[]byte{2, 3, 4}, 4, EQ},
+				ExpectType{[]byte{2, 3, 4, 5}, 5, EQ},
+				ExpectType{[]byte{2, 3, 15}, 6, EQ},
+				ExpectType{[]byte{2, 3, 4}, 3, LT},
+				ExpectType{[]byte{2, 3, 0}, 2, LT},
+				ExpectType{[]byte{2, 3, 0}, 3, LT | EQ},
+				ExpectType{[]byte{2, 3, 4}, 5, GT},
+				ExpectType{[]byte{2, 3, 6}, 6, GT},
+			},
+		},
+	}
+
+	for _, c := range cases {
+
+		for i, k := range c.key {
+			c.key[i] = wordKey(k)
+		}
+
+		trie := New(c.key, c.value)
+		for _, ex := range c.expected {
+			rst := trie.Search(wordKey(ex.key), ex.mode)
+
+			if !reflect.DeepEqual(ex.rst, rst) {
+				t.Error("ks: ", wordKey(ex.key), "expected value: ", ex.rst, "rst: ", rst, ex.mode)
+			}
+		}
+
+		ctrie := &CompactTrie{
+			Children: sparse.SparseArray{EltConverter: ChildConv{}},
+			Steps:    sparse.SparseArray{EltConverter: sparse.U16Conv{}},
+			Leaves:   sparse.SparseArray{EltConverter: sparse.U16Conv{}},
+		}
+
+		err := ctrie.Compact(trie)
+		if err != nil {
+			t.Error("compact trie error:", err)
+		}
+
+		for _, ex := range c.expected {
+			rst := ctrie.Search(wordKey(ex.key), ex.mode)
+
+			if !reflect.DeepEqual(ex.rst, rst) {
+				t.Fatal("ks: ", wordKey(ex.key), "expected value: ", ex.rst, "rst: ", rst, ex.mode)
+			}
+		}
+
+		trie.Squash()
+		trie.Squash()
+		for _, ex := range c.expected {
+			rst := trie.Search(wordKey(ex.key), ex.mode)
+
+			if !reflect.DeepEqual(ex.rst, rst) {
+				t.Error("ks: ", ex.key, "expected value: ", ex.rst, "rst: ", rst)
+			}
+		}
+
+		ctrie = &CompactTrie{
+			Children: sparse.SparseArray{EltConverter: ChildConv{}},
+			Steps:    sparse.SparseArray{EltConverter: sparse.U16Conv{}},
+			Leaves:   sparse.SparseArray{EltConverter: sparse.U16Conv{}},
+		}
+
+		err = ctrie.Compact(trie)
+		if err != nil {
+			t.Error("compact trie error:", err)
+		}
+
+		for _, ex := range c.expected {
+			rst := ctrie.Search(wordKey(ex.key), ex.mode)
+
+			if !reflect.DeepEqual(ex.rst, rst) {
+				t.Error("ks: ", ex.key, "expected value: ", ex.rst, "rst: ", rst)
+			}
+		}
+
+		trie.Squash()
+		trie.Squash()
+		for _, ex := range c.expected {
+			rst := trie.Search(wordKey(ex.key), ex.mode)
+
+			if !reflect.DeepEqual(ex.rst, rst) {
+				t.Error("ks: ", ex.key, "expected value: ", ex.rst, "rst: ", rst)
+			}
+		}
+
+		ctrie = &CompactTrie{
+			Children: sparse.SparseArray{EltConverter: ChildConv{}},
+			Steps:    sparse.SparseArray{EltConverter: sparse.U16Conv{}},
+			Leaves:   sparse.SparseArray{EltConverter: sparse.U16Conv{}},
+		}
+
+		err = ctrie.Compact(trie)
+		if err != nil {
+			t.Error("compact trie error:", err)
+		}
+
+		for _, ex := range c.expected {
+			rst := ctrie.Search(wordKey(ex.key), ex.mode)
+
+			if !reflect.DeepEqual(ex.rst, rst) {
+				t.Error("ks: ", ex.key, "expected value: ", ex.rst, "rst: ", rst)
+			}
+		}
+	}
+}
+
+func TestCompactTrieSearch(t *testing.T) {
+
+	var key = [][]byte{
+		{'a', 'b', 'c'},
+		{'a', 'b', 'c', 'd'},
+		{'a', 'b', 'd'},
+		{'a', 'b', 'd', 'e'},
+		{'b', 'c'},
+		{'b', 'c', 'd'},
+		{'b', 'c', 'd', 'e'},
+		{'c', 'd', 'e'},
+	}
+	var value = []interface{}{
+		[]byte{0},
+		[]byte{1},
+		[]byte{2},
+		[]byte{3},
+		[]byte{4},
+		[]byte{5},
+		[]byte{6},
+		[]byte{7},
+	}
+
+	type ExpectType struct {
+		mode Mode
+		rst  interface{}
+	}
+
+	for i, k := range key {
+		key[i] = wordKey(k)
+	}
+
+	var trie = New(key, value)
+
+	ctrie := &CompactTrie{
+		Children: sparse.SparseArray{EltConverter: ChildConv{}},
+		Steps:    sparse.SparseArray{EltConverter: sparse.U16Conv{}},
+		Leaves:   sparse.SparseArray{EltConverter: sparse.ByteConv{EltSize: uint32(1)}},
+	}
+
+	err := ctrie.Compact(trie)
+	if err != nil {
+		t.Error("compact trie error:", err)
+	}
+
+	var cases = []struct {
+		key      []byte
+		expected []ExpectType
+	}{
+		{
+			[]byte{'a', 'b', 'c'},
+			[]ExpectType{
+				{EQ, []byte{0}},
+				{LT | EQ, []byte{0}},
+				{LT, nil},
+				{GT | EQ, []byte{0}},
+				{GT, []byte{1}},
+			},
+		},
+		{
+			[]byte{'a', 'b', 'd'},
+			[]ExpectType{
+				{EQ, []byte{2}},
+				{LT | EQ, []byte{2}},
+				{LT, []byte{1}},
+				{GT | EQ, []byte{2}},
+				{GT, []byte{3}},
+			},
+		},
+		{
+			[]byte{'b', 'c', 'd'},
+			[]ExpectType{
+				{EQ, []byte{5}},
+				{LT | EQ, []byte{5}},
+				{LT, []byte{4}},
+				{GT | EQ, []byte{5}},
+				{GT, []byte{6}},
+			},
+		},
+		{
+			[]byte{'b', 'c', 'e'},
+			[]ExpectType{
+				{EQ, nil},
+				{LT | EQ, []byte{6}},
+				{LT, []byte{6}},
+				{GT | EQ, []byte{7}},
+				{GT, []byte{7}},
+			},
+		},
+		{
+			[]byte{'c', 'd', 'e'},
+			[]ExpectType{
+				{EQ, []byte{7}},
+				{LT | EQ, []byte{7}},
+				{LT, []byte{6}},
+				{GT | EQ, []byte{7}},
+				{GT, nil},
+			},
+		},
+		{
+			[]byte{'a', 'c', 'b'},
+			[]ExpectType{
+				{EQ, nil},
+				{LT | EQ, []byte{3}},
+				{LT, []byte{3}},
+				{GT | EQ, []byte{4}},
+				{GT, []byte{4}},
+			},
+		},
+		{
+			[]byte{'a', 'b'},
+			[]ExpectType{
+				{EQ, nil},
+				{LT | EQ, nil},
+				{LT, nil},
+				{GT | EQ, []byte{0}},
+				{GT, []byte{0}},
+			},
+		},
+		{
+			[]byte{'a', 'c'},
+			[]ExpectType{
+				{EQ, nil},
+				{LT | EQ, []byte{3}},
+				{LT, []byte{3}},
+				{GT | EQ, []byte{4}},
+				{GT, []byte{4}},
+			},
+		},
+		{
+			[]byte{'a', 'b', 'c', 'd', 'e'},
+			[]ExpectType{
+				{EQ, nil},
+				{LT | EQ, []byte{1}},
+				{LT, []byte{1}},
+				{GT | EQ, []byte{2}},
+				{GT, []byte{2}},
+			},
+		},
+	}
+
+	for _, c := range cases {
+
+		kk := wordKey(c.key)
+		for _, ex := range c.expected {
+
+			rst := trie.Search(kk, ex.mode)
+			if !reflect.DeepEqual(ex.rst, rst) {
+				t.Error("key: ", kk, "expected value: ", ex.rst, "rst: ", rst, "mode: ", ex.mode)
+			}
+
+			rst = ctrie.Search(kk, ex.mode)
+			if !reflect.DeepEqual(ex.rst, rst) {
+				t.Fatal("key: ", kk, "expected value: ", ex.rst, "rst: ", rst, "mode: ", ex.mode)
+			}
+		}
+	}
+
+	var squashedCases = []struct {
+		key      []byte
+		expected []ExpectType
+	}{
+		{
+			[]byte{'a', 'b'},
+			[]ExpectType{
+				{EQ, nil},
+				{LT | EQ, nil},
+				{LT, nil},
+				{GT | EQ, []byte{0}},
+				{GT, []byte{0}},
+			},
+		},
+		{
+			[]byte{'a', 'b', 'c'},
+			[]ExpectType{
+				{EQ, []byte{0}},
+				{LT | EQ, []byte{0}},
+				{LT, nil},
+				{GT | EQ, []byte{0}},
+				{GT, []byte{1}},
+			},
+		},
+		{
+			[]byte{'a', 'd', 'c'},
+			[]ExpectType{
+				{EQ, []byte{0}},
+				{LT | EQ, []byte{0}},
+				{LT, nil},
+				{GT | EQ, []byte{0}},
+				{GT, []byte{1}},
+			},
+		},
+		{
+			[]byte{'a', 'b', 'd'},
+			[]ExpectType{
+				{EQ, []byte{2}},
+				{LT | EQ, []byte{2}},
+				{LT, []byte{1}},
+				{GT | EQ, []byte{2}},
+				{GT, []byte{3}},
+			},
+		},
+		{
+			[]byte{'a', 'c', 'd'},
+			[]ExpectType{
+				{EQ, []byte{2}},
+				{LT | EQ, []byte{2}},
+				{LT, []byte{1}},
+				{GT | EQ, []byte{2}},
+				{GT, []byte{3}},
+			},
+		},
+		{
+			[]byte{'c', 'd', 'e'},
+			[]ExpectType{
+				{EQ, []byte{7}},
+				{LT | EQ, []byte{7}},
+				{LT, []byte{6}},
+				{GT | EQ, []byte{7}},
+				{GT, nil},
+			},
+		},
+		{
+			[]byte{'c', 'f', 'e'},
+			[]ExpectType{
+				{EQ, []byte{7}},
+				{LT | EQ, []byte{7}},
+				{LT, []byte{6}},
+				{GT | EQ, []byte{7}},
+				{GT, nil},
+			},
+		},
+		{
+			[]byte{'c', 'f', 'f'},
+			[]ExpectType{
+				{EQ, []byte{7}},
+				{LT | EQ, []byte{7}},
+				{LT, []byte{6}},
+				{GT | EQ, []byte{7}},
+				{GT, nil},
+			},
+		},
+		{
+			[]byte{'c'},
+			[]ExpectType{
+				{EQ, nil},
+				{LT | EQ, []byte{6}},
+				{LT, []byte{6}},
+				{GT | EQ, []byte{7}},
+				{GT, []byte{7}},
+			},
+		},
+		{
+			[]byte{'a', 'c'},
+			[]ExpectType{
+				{EQ, nil},
+				{LT | EQ, nil},
+				{LT, nil},
+				{GT | EQ, []byte{0}},
+				{GT, []byte{0}},
+			},
+		},
+		{
+			[]byte{'a', 'b', 'c', 'd', 'e'},
+			[]ExpectType{
+				{EQ, nil},
+				{LT | EQ, []byte{1}},
+				{LT, []byte{1}},
+				{GT | EQ, []byte{2}},
+				{GT, []byte{2}},
+			},
+		},
+	}
+
+	trie.Squash()
+
+	ctrie = &CompactTrie{
+		Children: sparse.SparseArray{EltConverter: ChildConv{}},
+		Steps:    sparse.SparseArray{EltConverter: sparse.U16Conv{}},
+		Leaves:   sparse.SparseArray{EltConverter: sparse.ByteConv{EltSize: uint32(1)}},
+	}
+
+	err = ctrie.Compact(trie)
+	if err != nil {
+		t.Error("compact trie error:", err)
+	}
+
+	for _, c := range squashedCases {
+
+		kk := wordKey(c.key)
+		for _, ex := range c.expected {
+
+			rst := trie.Search(kk, ex.mode)
+			if !reflect.DeepEqual(ex.rst, rst) {
+				t.Error("key: ", kk, "expected value: ", ex.rst, "rst: ", rst, "mode: ", ex.mode)
+			}
+
+			rst = ctrie.Search(kk, ex.mode)
+			if !reflect.DeepEqual(ex.rst, rst) {
+				t.Fatal("key: ", kk, "expected value: ", ex.rst, "rst: ", rst, "mode: ", ex.mode)
+			}
+		}
+	}
+}
