690cb33882f4506a4eeb625534652a142edcf39b
Li, Shulong <shulong.li@baishancloud.com>
2018-06-09 17:18:45 +0800

    adjust L1 Index according to doc/block.md

--- trie/l1_index.go
+++ trie/l1_index.go
@@ -4,6 +4,12 @@ import (
 	"xec/prototype"
 )
 
+/**
+ * Keys                []string
+ * L2IdxOffsets        []int64
+ * NeedleOffsets       []int64
+ * NeedleIDListOffsets []int64  offsets relative to the start addr of NeedleIDList
+ */
 type L1Index struct {
 	prototype.L1Index
 }
@@ -11,28 +17,38 @@ type L1Index struct {
 func NewL1Index() *L1Index {
 	return &L1Index{
 		L1Index: prototype.L1Index{
-			Keys:    make([]string, 0),
-			Offsets: make([]int64, 0),
+			Keys:                make([]string, 0),
+			L2IdxOffsets:        make([]int64, 0),
+			NeedleOffsets:       make([]int64, 0),
+			NeedleIDListOffsets: make([]int64, 0),
 		},
 	}
 }
 
-func (l1Idx *L1Index) Add(key string, offset int64) {
+func (l1Idx *L1Index) Add(key string, l2Offset, ndlOffset, ndlIDLOffset int64) {
 	l1Idx.Keys = append(l1Idx.Keys, key)
-	l1Idx.Offsets = append(l1Idx.Offsets, offset)
+	l1Idx.L2IdxOffsets = append(l1Idx.L2IdxOffsets, l2Offset)
+	l1Idx.NeedleOffsets = append(l1Idx.NeedleOffsets, ndlOffset)
+	l1Idx.NeedleIDListOffsets = append(l1Idx.NeedleIDListOffsets, ndlIDLOffset)
 }
 
-func (l1Idx *L1Index) RPop() (key string, offset int64, ok bool) {
+func (l1Idx *L1Index) RPop() (key string, l2Offset, ndlOffset, ndlIDLOffset int64, ok bool) {
 	length := len(l1Idx.Keys)
 	if length == 0 {
-		return key, offset, false
+		return key, l2Offset, ndlOffset, ndlIDLOffset, false
 	}
 
 	key = l1Idx.Keys[length-1]
 	l1Idx.Keys = l1Idx.Keys[:length-1]
 
-	offset = l1Idx.Offsets[length-1]
-	l1Idx.Offsets = l1Idx.Offsets[:length-1]
+	l2Offset = l1Idx.L2IdxOffsets[length-1]
+	l1Idx.L2IdxOffsets = l1Idx.L2IdxOffsets[:length-1]
 
-	return key, offset, true
+	ndlOffset = l1Idx.NeedleOffsets[length-1]
+	l1Idx.NeedleOffsets = l1Idx.NeedleOffsets[:length-1]
+
+	ndlIDLOffset = l1Idx.NeedleIDListOffsets[length-1]
+	l1Idx.NeedleIDListOffsets = l1Idx.NeedleIDListOffsets[:length-1]
+
+	return key, l2Offset, ndlOffset, ndlIDLOffset, true
 }
