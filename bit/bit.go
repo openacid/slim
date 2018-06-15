28d449cfdab7688ebcbf2d4179518225b6236bfe
templexxx <lucas1x1x@gmail.com>
2018-06-15 08:35:08 +0800

    调整代码结构和命名
    1. 与标准库避开冲突
    2. 拆分 blocksvr/storage.go 进入  block chunk
    3. 合并 rs xrs xor stream 进入 ec
    4. 命名调整
    5. 将 io 相关函数移入 xio
    6. 错误字调整

--- bit/bit.go
+++ bit/bit.go
@@ -6,12 +6,12 @@ import (
 
 func Cnt1Before(n uint64, iBit uint32) uint32 {
 	var cnt uint32 = 0
-	MaxBit := uint32(unsafe.Sizeof(n)) * 8
+	maxBit := uint32(unsafe.Sizeof(n)) * 8
 	if iBit == 0 {
 		n = 0
 	} else {
-		if iBit < MaxBit {
-			n <<= MaxBit - iBit
+		if iBit < maxBit {
+			n <<= maxBit - iBit
 		}
 	}
 
