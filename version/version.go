1ad25d8aff6fa8d14cafdef793662ab472373101
drdr xp <drdr.xp@gmail.com>
2019-02-13 14:28:05 +0800

    version: add doc

--- version/version.go
+++ version/version.go
@@ -1,7 +1,17 @@
+// Package version defines version of this repo.
+//
+// It provides backward compatibility checking.
+// Most data structure defined in this repo provides with
+// serializing/deserializing APIs.
+// Serialized data has a version to describe its structure.
 package version
 
 const (
+	// Latest supported version.
+	//
+	// On-disk data structure with older version can be read by a newer program.
 	VERSION = "1.0.0"
+
 	/**
 	 * never change MAXLEN, it is long enough
 	 *
@@ -9,5 +19,9 @@ const (
 	 * one byte smaller than MAXLEN because having to make room
 	 * for delimiter in byte array
 	 */
+
+	// Fixed size in byte for serialized version.
+	//
+	// verion is serialized as a string with a trailing '\0'.
 	MAXLEN = 16
 )
