36887a1411d7c182e6581118e4ba65709612d09b
drdr xp <drdr.xp@gmail.com>
2019-02-21 14:29:16 +0800

    prototype: add auto generate config

---
+++ prototype/prototype.go
@@ -0,0 +1,9 @@
+// Package pb provides generated Protocol Buffers for cache serialization.
+//
+// Usage:
+//		go generate ./...
+//
+// It just read the comment line starts with "//go:generate" and run.
+package prototype
+
+//go:generate protoc --proto_path=./proto --go_out=. proto/compacted_array.proto
