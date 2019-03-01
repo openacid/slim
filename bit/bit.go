f3f879c37dfc5cee230e66cc13d4268026ee8a75
drdr xp <drdr.xp@gmail.com>
2019-03-01 18:05:49 +0800

    bit: add counter for 32, 16 an 8; add test and bench

--- bit/bit.go
+++ bit/bit.go
@@ -1,10 +1,25 @@
 // Package bit provides efficient bitwise operations on integer numbers.
 package bit
 
-const m1 uint64 = 0x5555555555555555  // binary: 0101...
-const m2 uint64 = 0x3333333333333333  // binary: 00110011...
-const m4 uint64 = 0x0f0f0f0f0f0f0f0f  // binary: 0000111100001111...
-const h01 uint64 = 0x0101010101010101 // the sum of 256 to the power of 0,1,2,3...
+const (
+	m64_1 uint64 = 0x5555555555555555 // binary: 0101...
+	m64_2 uint64 = 0x3333333333333333 // binary: 00110011...
+	m64_4 uint64 = 0x0f0f0f0f0f0f0f0f // binary: 0000111100001111...
+	h64_8 uint64 = 0x0101010101010101 // binary: 0000000100000001...
+
+	m32_1 uint32 = 0x55555555 // binary: 0101...
+	m32_2 uint32 = 0x33333333 // binary: 00110011...
+	m32_4 uint32 = 0x0f0f0f0f // binary: 0000111100001111...
+	h32_8 uint32 = 0x01010101 // binary: 0000000100000001...
+
+	m16_1 uint16 = 0x5555 // binary: 0101...
+	m16_2 uint16 = 0x3333 // binary: 00110011...
+	h16_4 uint16 = 0x0111 // binary: 0000000100010001
+
+	m8_1 uint8 = 0x55 // binary: 01010101
+	m8_2 uint8 = 0x33 // binary: 00110011
+	h8_4 uint8 = 0x11 // binary: 00010001
+)
 
 // PopCnt64Before counts the number of "1" before specified bit position `iBit`.
 //
@@ -21,11 +36,47 @@ const h01 uint64 = 0x0101010101010101 // the sum of 256 to the power of 0,1,2,3.
 // This algorithm has more introduction in:
 // https://en.wikipedia.org/wiki/Hamming_weight#Efficient_implementation
 func PopCnt64Before(n uint64, iBit uint32) uint32 {
-	n = n & ((1 << iBit) - 1)
+	n = n & ((uint64(1) << iBit) - 1)
 
-	n -= (n >> 1) & m1             // put count of each 2 bits into those 2 bits
-	n = (n & m2) + ((n >> 2) & m2) // put count of each 4 bits into those 4 bits
-	n = (n + (n >> 4)) & m4        // put count of each 8 bits into thoes 8 bits
+	n -= (n >> 1) & m64_1                // put count of each 2 bits into those 2 bits
+	n = (n & m64_2) + ((n >> 2) & m64_2) // put count of each 4 bits into those 4 bits
+	n = (n + (n >> 4)) & m64_4           // put count of each 8 bits into thoes 8 bits
 
-	return uint32((n * h01) >> 56) // returns left 8 bits of x + (x << 8) + (x << 16) + (x<<24) + ...
+	return uint32((n * h64_8) >> 56) // returns left 8 bits of x + (x << 8) + (x << 16) + (x<<24) + ...
+}
+
+// PopCnt32Before is similar to PopCnt64Before, except it accepts a uint32 `n`
+func PopCnt32Before(n uint32, iBit uint32) int {
+	n = n & ((uint32(1) << iBit) - 1)
+
+	n -= (n >> 1) & m32_1
+	n = (n & m32_2) + ((n >> 2) & m32_2)
+	n = (n + (n >> 4)) & m32_4
+
+	return int((n * h32_8) >> 24)
+}
+
+// PopCnt16Before is similar to PopCnt64Before, except it accepts a uint16 `n`
+// instead of a uint64 `n`.
+// It is about 2.5% faster than `PopCnt64Before(uint16(i), iBit)`.
+func PopCnt16Before(n uint16, iBit uint32) int {
+	n = n & ((uint16(1) << iBit) - 1)
+
+	n -= (n >> 1) & m16_1
+	n = (n & m16_2) + ((n >> 2) & m16_2)
+
+	// Because 4 4-bit segments have at most 16 ones.
+	// It is not enought for the left most 4 bit to store the count.
+	return int(((n * h16_4) >> 12) + (n & 0x0f))
+}
+
+// PopCnt8Before is similar to PopCnt64Before, except it accepts a uint8 `n`
+// It is about 5% faster than `PopCnt64Before(uint16(i), iBit)`.
+func PopCnt8Before(n uint8, iBit uint32) int {
+	n = n & ((uint8(1) << iBit) - 1)
+
+	n -= (n >> 1) & m8_1
+	n = (n & m8_2) + ((n >> 2) & m8_2)
+
+	return int((n * h8_4) >> 4)
 }
--- bit/bit_test.go
+++ bit/bit_test.go
@@ -1,6 +1,11 @@
-package bit
+package bit_test
 
-import "testing"
+import (
+	gobits "math/bits"
+	"testing"
+
+	"github.com/openacid/slim/bit"
+)
 
 func TestPopCnt64Before(t *testing.T) {
 	var cases = []struct {
@@ -8,17 +13,111 @@ func TestPopCnt64Before(t *testing.T) {
 		iBit uint32
 		expN uint32
 	}{
-		{0, 0, 0},   // 0b00
-		{1, 0, 0},   // 0b01
-		{2, 1, 0},   // 0b10
-		{3, 1, 1},   // 0b11
-		{3, 2, 2},   // 0b11
-		{3, 4, 2},   // 0b11
-		{32, 10, 1}, // 0b10000
+		{0, 0, 0},                    // 0b00
+		{1, 0, 0},                    // 0b01
+		{2, 1, 0},                    // 0b10
+		{3, 1, 1},                    // 0b11
+		{3, 2, 2},                    // 0b11
+		{3, 4, 2},                    // 0b11
+		{32, 10, 1},                  // 0b10000
+		{0xffffffffffffffff, 0, 0},   // 0b11....
+		{0xffffffffffffffff, 1, 1},   // 0b11....
+		{0xffffffffffffffff, 2, 2},   // 0b11....
+		{0xffffffffffffffff, 63, 63}, // 0b11....
+		{0xffffffffffffffff, 64, 64}, // 0b11....
+		{0xffffffffffffffff, 65, 64}, // 0b11....
+		{0xffffffffffffffff, 66, 64}, // 0b11....
 	}
 	for _, c := range cases {
 		n, iBit, expN := c.n, c.iBit, c.expN
-		actN := PopCnt64Before(n, iBit)
+		actN := bit.PopCnt64Before(n, iBit)
+		if actN != expN {
+			t.Fatalf("failed, case:%+v, actN:%d", c, actN)
+		}
+	}
+}
+
+func TestPopCnt32Before(t *testing.T) {
+	var cases = []struct {
+		n    uint32
+		iBit uint32
+		expN int
+	}{
+		{0, 0, 0},            // 0b00
+		{1, 0, 0},            // 0b01
+		{2, 1, 0},            // 0b10
+		{3, 1, 1},            // 0b11
+		{3, 2, 2},            // 0b11
+		{3, 4, 2},            // 0b11
+		{32, 10, 1},          // 0b10000
+		{0xffffffff, 0, 0},   // 0b11....
+		{0xffffffff, 1, 1},   // 0b11....
+		{0xffffffff, 2, 2},   // 0b11....
+		{0xffffffff, 31, 31}, // 0b11....
+		{0xffffffff, 32, 32}, // 0b11....
+		{0xffffffff, 33, 32}, // 0b11....
+		{0xffffffff, 33, 32}, // 0b11....
+	}
+	for _, c := range cases {
+		n, iBit, expN := c.n, c.iBit, c.expN
+		actN := bit.PopCnt32Before(n, iBit)
+		if actN != expN {
+			t.Fatalf("failed, case:%+v, actN:%d", c, actN)
+		}
+	}
+}
+
+func TestPopCnt16Before(t *testing.T) {
+	var cases = []struct {
+		n    uint16
+		iBit uint32
+		expN int
+	}{
+		{0, 0, 0},        // 0b00
+		{1, 0, 0},        // 0b01
+		{2, 1, 0},        // 0b10
+		{3, 1, 1},        // 0b11
+		{3, 2, 2},        // 0b11
+		{3, 4, 2},        // 0b11
+		{32, 10, 1},      // 0b10000
+		{0xffff, 0, 0},   // 0b11....
+		{0xffff, 1, 1},   // 0b11....
+		{0xffff, 2, 2},   // 0b11....
+		{0xffff, 15, 15}, // 0b11....
+		{0xffff, 16, 16}, // 0b11....
+		{0xffff, 17, 16}, // 0b11....
+	}
+	for _, c := range cases {
+		n, iBit, expN := c.n, c.iBit, c.expN
+		actN := bit.PopCnt16Before(n, iBit)
+		if actN != expN {
+			t.Fatalf("failed, case:%+v, actN:%d", c, actN)
+		}
+	}
+}
+
+func TestPopCnt8Before(t *testing.T) {
+	var cases = []struct {
+		n    uint8
+		iBit uint32
+		expN int
+	}{
+		{0, 0, 0},    // 0b00
+		{1, 0, 0},    // 0b01
+		{2, 1, 0},    // 0b10
+		{3, 1, 1},    // 0b11
+		{3, 2, 2},    // 0b11
+		{3, 4, 2},    // 0b11
+		{0xff, 0, 0}, // 0b11....
+		{0xff, 1, 1}, // 0b11....
+		{0xff, 2, 2}, // 0b11....
+		{0xff, 7, 7}, // 0b11....
+		{0xff, 8, 8}, // 0b11....
+		{0xff, 9, 8}, // 0b11....
+	}
+	for _, c := range cases {
+		n, iBit, expN := c.n, c.iBit, c.expN
+		actN := bit.PopCnt8Before(n, iBit)
 		if actN != expN {
 			t.Fatalf("failed, case:%+v, actN:%d", c, actN)
 		}
@@ -30,6 +129,61 @@ func BenchmarkPopCnt64Before(b *testing.B) {
 	var n uint64 = 12334567890
 
 	for i := 0; i < b.N; i++ {
-		PopCnt64Before(n+uint64(i), uint32(i)%64)
+		bit.PopCnt64Before(n+uint64(i), uint32(i)%64)
+	}
+}
+
+func BenchmarkPopCnt32Before(b *testing.B) {
+
+	var n uint32 = 123345678
+
+	for i := 0; i < b.N; i++ {
+		bit.PopCnt32Before(n+uint32(i), uint32(i)%32)
+	}
+}
+
+func BenchmarkPopCnt16(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		bit.PopCnt16Before(uint16(i), uint32(i)%16)
+	}
+}
+
+func BenchmarkPopCnt8(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		bit.PopCnt8Before(uint8(i), uint32(i)%8)
+	}
+}
+
+func BenchmarkGoPopCnt64Before(b *testing.B) {
+
+	var n uint64 = 12334567890
+
+	for i := 0; i < b.N; i++ {
+		gobits.OnesCount64(n + uint64(i))
+	}
+}
+
+func BenchmarkGoPopCnt32Before(b *testing.B) {
+
+	var n uint32 = 123345678
+
+	for i := 0; i < b.N; i++ {
+		gobits.OnesCount32(n + uint32(i))
+	}
+}
+
+func BenchmarkGoPopCnt16(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		gobits.OnesCount16(uint16(i))
+	}
+}
+
+func BenchmarkGoPopCnt8(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		gobits.OnesCount8(uint8(i))
 	}
 }
