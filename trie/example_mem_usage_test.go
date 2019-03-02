2576c55ba1a25294df83cce2be663cf4b04344bc
drdr xp <drdr.xp@gmail.com>
2019-03-02 21:52:26 +0800

    trie example: one for usage and one for memory consumption test.

---
+++ trie/example_mem_usage_test.go
@@ -0,0 +1,78 @@
+package trie_test
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/openacid/slim/marshal"
+	"github.com/openacid/slim/trie"
+)
+
+var (
+	m = marshal.String16{}
+)
+
+func makeCopies(ncopy int, keys []string, offsets []uint16) []*trie.SlimTrie {
+	copies := make([]*trie.SlimTrie, ncopy)
+
+	for i := 0; i < len(copies); i++ {
+		copies[i], _ = trie.NewSlimTrie(marshal.U16{}, keys, offsets)
+	}
+	return copies
+}
+
+func Example_memoryUsage() {
+
+	data := make([]byte, 0)
+
+	keys := []string{}
+	offsets := []uint16{}
+	ksize := 0
+
+	for i, k := range words2 {
+		v := fmt.Sprintf("is the %d-th word", i)
+
+		keys = append(keys, k)
+		offsets = append(offsets, uint16(len(data)))
+
+		data = append(data, m.Marshal(k)...)
+		data = append(data, m.Marshal(v)...)
+
+		ksize += len(k)
+	}
+
+	ncopy := 100
+	vsize := 2.0
+
+	rss1 := readRss()
+	copies := makeCopies(ncopy, keys, offsets)
+	rss2 := readRss()
+
+	diff := float64(rss2 - rss1)
+
+	avgIdxLen := diff/float64(ncopy)/float64(len(keys)) - vsize
+	avgKeyLen := float64(ksize) / float64(len(keys))
+
+	ratio := avgIdxLen / avgKeyLen * 100
+
+	fmt.Printf(
+		"Orignal:: %.1f byte/key --> SlimTrie index: %.1f byte/index\n"+
+			"Saved %.1f%%",
+		avgKeyLen,
+		avgIdxLen,
+		100-ratio,
+	)
+
+	for _, cc := range copies {
+		_ = cc.Children
+	}
+}
+
+func readRss() int64 {
+	var stats runtime.MemStats
+
+	runtime.GC()
+	runtime.ReadMemStats(&stats)
+
+	return int64(stats.Alloc)
+}
--- trie/example_test.go
+++ trie/example_test.go
@@ -1,5 +1,66 @@
 package trie_test
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/openacid/slim/trie"
+)
+
+type Data string
+
+func (d Data) Read(offset int64, key string) (string, bool) {
+	kv := strings.Split(string(d)[offset:], ",")[0:2]
+	if kv[0] == key {
+		return kv[1], true
+	}
+	return "", false
+}
+
 func Example() {
 
+	// SlimTrie is a memory efficient index data type.
+	//
+	// In this example, we show how to accelerate external data accessing
+	// (in memory or on disk) by indexing them with a SlimTrie:
+	//
+	// `data` is a sample of some unindexed data. In our example it is a comma
+	// seperated key value series.
+	//
+	// In order to let SlimTrie be able to read data, `data` should have
+	// a `Read` method:
+	//     Read(offset int64, key string) (string, bool)
+	data := Data("Aaron,1,Agatha,1,Al,2,Albert,3,Alexander,5,Alison,8")
+
+	// `index` is a prebuilt index that stores key and its offset in data accordingly.
+	index := []trie.OffsetIndexItem{
+		{Key: "Aaron", Offset: 0},
+		{Key: "Agatha", Offset: 8},
+		{Key: "Al", Offset: 17},
+		{Key: "Albert", Offset: 22},
+		{Key: "Alexander", Offset: 31},
+		{Key: "Alison", Offset: 43},
+	}
+
+	// Create a index `trie.SlimIndex`, which is simply a container of SlimTrie
+	// and its data.
+	st, err := trie.NewSlimIndex(index, data)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Lookup
+	v, found := st.Get("Alison")
+	fmt.Printf("key: %q\n  found: %t\n  value: %q\n", "Alison", found, v)
+
+	v, found = st.Get("foo")
+	fmt.Printf("key: %q\n  found: %t\n  value: %q\n", "foo", found, v)
+
+	// Output:
+	// key: "Alison"
+	//   found: true
+	//   value: "8"
+	// key: "foo"
+	//   found: false
+	//   value: ""
 }
