59498f78018ba6b20682520e2163157e5cc04c6e
drdr xp <drdr.xp@gmail.com>
2019-03-30 01:57:49 +0800

    refactor: array: simplify error definition

--- array/errors.go
+++ array/errors.go
@@ -4,24 +4,16 @@ import (
 	"github.com/openacid/errors"
 )
 
-// ErrIndexNotAscending indicates that the indexes to initialize an Array is not
-// in ascending order.
-//
-// Since 0.2.0
-var ErrIndexNotAscending = errors.New("index must be an ascending ordered slice")
+var (
+	// ErrIndexNotAscending indicates that the indexes to initialize an Array is not
+	// in ascending order.
+	//
+	// Since 0.2.0
+	ErrIndexNotAscending = errors.New("index must be an ascending ordered slice")
 
-// ErrIndexLen indicates that the number of indexes does not equal the number of
-// elements, when initializing an Array.
-//
-// Since 0.2.0
-var ErrIndexLen = errors.New("the length of indexes and elts must be equal")
-
-// ErrUnknownSize indicates that the element is not a fixed-type.
-//
-// Since 0.2.0
-var ErrUnknownSize = errors.New("the size of array size is unknown")
-
-// ErrDifferentEltSize indicates that two elements have different size.
-//
-// Since 0.2.0
-var ErrDifferentEltSize = errors.New("elements have different size")
+	// ErrIndexLen indicates that the number of indexes does not equal the number of
+	// elements, when initializing an Array.
+	//
+	// Since 0.2.0
+	ErrIndexLen = errors.New("the length of indexes and elts must be equal")
+)
