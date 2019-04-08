f3b5848b80e909642a7a066b4d42e20fe10b137c
drdr xp <drdr.xp@gmail.com>
2019-04-08 21:26:15 +0800

    doc: array: auto-generate report and chart

--- array/report/report.go
+++ array/report/report.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/openacid/slim/array"
@@ -48,79 +50,119 @@ func memUsage() {
 	table.Render()
 }
 
+var Output int
+
 func benGet() {
 
-	var rst testing.BenchmarkResult
-
-	fn := "report/bench_get.md"
-	f, table := benchhelper.NewMDFileTable(fn)
-	defer f.Close()
-
-	table.SetHeader([]string{"Elt-Type", "Elt-Count", "ns/get"})
-	table.SetColumnAlignment(">>>>")
+	rows := [][]string{}
 
 	ns := []int{1, 256, 65536}
 
 	for _, n := range ns {
 
-		{
-			indexes := make([]int32, n)
-			elts := make([]uint16, n)
-			for i := 0; i < n; i++ {
-				indexes[i] = int32(i)
-			}
+		indexes := make([]int32, n)
+		for i := 0; i < n; i++ {
+			indexes[i] = int32(i)
+		}
 
-			a, err := array.NewU16(indexes, elts)
+		row := []string{fmt.Sprintf("%d", n)}
+
+		{
+			a, err := array.NewU16(indexes, make([]uint16, n))
 			if err != nil {
 				panic(err)
 			}
-
-			rst = testing.Benchmark(func(b *testing.B) {
+			rst := testing.Benchmark(func(b *testing.B) {
+				var s uint16
 				for i := 0; i < b.N; i++ {
-					a.Get(int32(i % n))
+					v, _ := a.Get(int32(i % n))
+					s += v
 				}
+				Output = int(s)
 			})
-			row := []string{
-				"uint16(2)",
-				fmt.Sprintf("%d", n),
-				fmt.Sprintf("%d", rst.NsPerOp()),
-			}
-
-			table.Append(row)
+			row = append(row, fmt.Sprintf("%d", rst.NsPerOp()))
 		}
 
 		{
-			indexes := make([]int32, n)
-			elts := make([]uint64, n)
-			for i := 0; i < n; i++ {
-				indexes[i] = int32(i)
-			}
-
-			a, err := array.NewU64(indexes, elts)
+			a, err := array.NewU32(indexes, make([]uint32, n))
 			if err != nil {
 				panic(err)
 			}
-
-			rst = testing.Benchmark(func(b *testing.B) {
+			rst := testing.Benchmark(func(b *testing.B) {
+				var s uint32
 				for i := 0; i < b.N; i++ {
-					a.Get(int32(i % n))
+					v, _ := a.Get(int32(i % n))
+					s += v
 				}
+				Output = int(s)
 			})
 
-			row := []string{
-				"uint64(8)",
-				fmt.Sprintf("%d", n),
-				fmt.Sprintf("%d", rst.NsPerOp()),
-			}
-
-			table.Append(row)
+			row = append(row, fmt.Sprintf("%d", rst.NsPerOp()))
 		}
+
+		{
+			a, err := array.NewU64(indexes, make([]uint64, n))
+			if err != nil {
+				panic(err)
+			}
+			rst := testing.Benchmark(func(b *testing.B) {
+				var s uint64
+				for i := 0; i < b.N; i++ {
+					v, _ := a.Get(int32(i % n))
+					s += v
+				}
+				Output = int(s)
+			})
+
+			row = append(row, fmt.Sprintf("%d", rst.NsPerOp()))
+		}
+
+		rows = append(rows, row)
+
 	}
 
+	fn := "report/bench_get.md"
+	f, table := benchhelper.NewMDFileTable(fn)
+	defer f.Close()
+
+	table.SetHeader([]string{"key-count", "u16", "u32", "u64"})
+	table.SetColumnAlignment(">>>>")
+	table.AppendBulk(rows)
 	table.Render()
+
+	lines := []string{}
+	for _, row := range rows {
+		line := strings.Join(row, " ")
+		lines = append(lines, line)
+	}
+
+	cont := strings.Join(lines, "\n")
+
+	fn = "report/bench_get.data"
+	err := ioutil.WriteFile(fn, []byte(cont), 0777)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
+	fmt.Println("generating mem usage benchmark...")
 	memUsage()
+
+	fmt.Println("generating get() benchmark...")
 	benGet()
+
+	fmt.Println("generating get() benchmark chart...")
+	scriptGet := benchhelper.Fformat.JPGHistogram +
+		benchhelper.LineStyles.Green + `
+set yr [5:20]
+set xlabel 'key-count (n)'
+set ylabel 'Get() ns/op' offset 1,0
+
+plot "report/bench_get.data" using 2:xtic(1) with histogram linestyle 1 title "u16", \
+     ''						 using 3:xtic(1) with histogram linestyle 2 title "u32", \
+     ''						 using 4:xtic(1) with histogram linestyle 3 title "u64"
+`
+
+	benchhelper.Fplot("report/bench_get.jpg", scriptGet)
 }
---
+++ benchhelper/plot.go
@@ -0,0 +1,83 @@
+package benchhelper
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"os/exec"
+)
+
+var Fformat = struct {
+	JPGHistogram string
+}{
+	JPGHistogram: `
+set terminal jpeg size 400,300;
+set boxwidth 0.8;
+set style fill solid;
+set grid ytics;
+`,
+}
+
+var LineStyles = struct {
+	Green  string
+	Yellow string
+}{
+	Green: `
+set style line 1 lc rgb '#aaffa5' pt 1 ps 1 lt 1 lw 2;
+set style line 2 lc rgb '#99f094' pt 6 ps 1 lt 1 lw 2;
+set style line 3 lc rgb '#87e082' pt 6 ps 1 lt 1 lw 2;
+set style line 4 lc rgb '#76d171' pt 6 ps 1 lt 1 lw 2;
+set style line 5 lc rgb '#65c260' pt 6 ps 1 lt 1 lw 2;
+set style line 6 lc rgb '#54b34f' pt 6 ps 1 lt 1 lw 2;
+set style line 7 lc rgb '#42a33d' pt 6 ps 1 lt 1 lw 2;
+set style line 8 lc rgb '#31942c' pt 6 ps 1 lt 1 lw 2;
+`,
+	Yellow: `
+set style line 1  lc rgb '#fffc85' pt 1 ps 1 lt 1 lw 2;
+set style line 2  lc rgb '#fdf476' pt 6 ps 1 lt 1 lw 2;
+set style line 3  lc rgb '#fbed67' pt 6 ps 1 lt 1 lw 2;
+set style line 4  lc rgb '#f8e559' pt 6 ps 1 lt 1 lw 2;
+set style line 5  lc rgb '#f6dd4a' pt 6 ps 1 lt 1 lw 2;
+set style line 6  lc rgb '#f4d63b' pt 6 ps 1 lt 1 lw 2;
+set style line 7  lc rgb '#f2ce2c' pt 6 ps 1 lt 1 lw 2;
+set style line 8  lc rgb '#efc61e' pt 6 ps 1 lt 1 lw 2;
+set style line 9  lc rgb '#edbf0f' pt 6 ps 1 lt 1 lw 2;
+set style line 10 lc rgb '#ebb700' pt 6 ps 1 lt 1 lw 2;
+`,
+}
+
+// Plot a image by gnuplot script "script" and output it to "fn".
+func Fplot(fn, script string) {
+	gp := exec.Command("gnuplot")
+	stdin, err := gp.StdinPipe()
+	if err != nil {
+		panic(err)
+	}
+
+	var stdout bytes.Buffer
+	gp.Stdout = &stdout
+
+	err = gp.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = io.WriteString(stdin, script)
+	if err != nil {
+		panic(err)
+	}
+	err = stdin.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	err = gp.Wait()
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(fn, stdout.Bytes(), 0777)
+	if err != nil {
+		panic(err)
+	}
+}
