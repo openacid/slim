5b8af3375b79e6f4506e3e31e10af2da2d64fbeb
drdr xp <drdr.xp@gmail.com>
2019-04-19 00:54:56 +0800

    doc: script: add change-log builder

---
+++ scripts/build_change_log.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import subprocess32
+import semantic_version
+import yaml
+from collections import defaultdict
+
+categories = {
+        'api-changes:' : 'api-changes',
+        'new-feature:': 'new-feature',
+        'doc:': 'doc',
+        'refactor:': 'refactor',
+}
+
+to_display = {
+        'doc': False,
+        'refactor': False,
+}
+
+def cmd(cmds):
+    subproc = subprocess32.Popen(cmds,
+                                  stdout=subprocess32.PIPE,
+                                  stderr=subprocess32.PIPE, )
+    out, err = subproc.communicate()
+    subproc.wait()
+
+    code = subproc.returncode
+    if code != 0:
+        raise OSError(out + "\n" + err)
+
+    return out
+
+def list_tags():
+    out = cmd(["git", "tag", "-l"])
+    tags = out.splitlines()
+    tags = [semantic_version.Version(t.lstrip('v'))
+            for t in tags
+            if t != '' ]
+    return tags
+
+
+def changes(frm, to):
+    # subject, author time, author name, email.
+    out = cmd(["git", "log", '--format=%s ||| %ai ||| %an ||| %ae', '--reverse', frm + '..' + to])
+    lines = out.split("\n")
+    lines = [x for x in lines if x != '']
+    rst = []
+    for line in lines:
+        elts = line.split(" ||| ")
+        item = {
+                'subject': elts[0],
+                # 2019-04-18 13:36:42 +0800
+                'time': elts[1].split()[0],
+                'author': elts[2],
+                'email': elts[3]
+        }
+        rst.append(item)
+
+    return rst
+
+def norm_changes(changes):
+    rst = {}
+    for ch in changes:
+        sub = ch['subject']
+        cate, mod, cont = sub.split(' ', 2)
+        catetitle = categories[cate]
+        mod = mod.rstrip(':')
+
+        if catetitle not in rst:
+            rst[catetitle] = {}
+
+        c = rst[catetitle]
+        if mod not in c:
+            c[mod] = []
+
+        l = c[mod]
+        desc = {
+                "content": cont.replace(':', ''),
+                "time": ch['time'],
+                "author": ch['author'],
+                'email': ch['email'],
+        }
+        l.append('{content}; by {author}; {time}'.format(**desc))
+
+    return rst
+
+def doit(newver):
+    tags = list_tags()
+    tags.sort()
+
+    newver = newver.lstrip('v')
+    newver = semantic_version.Version(newver)
+    tags = [t for t in tags if t < newver]
+    latest = tags[-1]
+
+    chs = changes('v' + str(latest), 'HEAD')
+    chs = norm_changes(chs)
+    chs = {k:v for k, v in chs.items() if to_display.get(k, True) }
+
+    changelog = yaml.dump(chs, default_flow_style=False)
+
+    with open('docs/change-log/v{newver}.yaml'.format(newver=newver), 'w') as f:
+        f.write(changelog)
+
+if __name__ == "__main__":
+    newver = sys.argv[1]
+    doit(newver)
