1647fe6302198f83eb7adf2faa56fb5b2d081c34
drdr xp <drdr.xp@gmail.com>
2019-05-17 16:56:17 +0800

    new-feature: strhelper: add ToBin() converts integer or slice of integer to binary format string

--- strhelper/strhelper.go
+++ strhelper/strhelper.go
@@ -1,6 +1,13 @@
 // Package strhelper provides string operations functions
 package strhelper
 
+import (
+	"fmt"
+	"math/bits"
+	"reflect"
+	"strings"
+)
+
 var (
 	// mask for 1, 2, 4, 8 bit word
 	wordMask = []byte{
@@ -82,4 +89,61 @@ func SliceFromBitWords(bytesslice [][]byte, n int) []string {
 		rst[i] = FromBitWords(s, n)
 	}
 	return rst
+}
+
+// ToBin converts integer or slice of integer to binary format string.
+// Significant bits are at right.
+// E.g.:
+//    int32(0x0102) --> 01000000 10000000
+func ToBin(x interface{}) string {
+
+	rst := []string{}
+
+	v := reflect.ValueOf(x)
+	if v.Kind() == reflect.Slice {
+		n := v.Len()
+		for i := 0; i < n; i++ {
+			rst = append(rst, intToBin(v.Index(i).Interface()))
+		}
+		return strings.Join(rst, ",")
+
+	} else {
+		return intToBin(x)
+	}
+}
+
+func intToBin(i interface{}) string {
+
+	sz, v := intSize(i)
+
+	rst := []string{}
+	for i := 0; i < sz; i++ {
+		b := uint8(v >> uint(i*8))
+		s := fmt.Sprintf("%08b", bits.Reverse8(b))
+		rst = append(rst, s)
+	}
+	return strings.Join(rst, " ")
+}
+
+func intSize(i interface{}) (int, uint64) {
+	switch i := i.(type) {
+	case int8:
+		return 1, uint64(i)
+	case uint8:
+		return 1, uint64(i)
+	case int16:
+		return 2, uint64(i)
+	case uint16:
+		return 2, uint64(i)
+	case int32:
+		return 4, uint64(i)
+	case uint32:
+		return 4, uint64(i)
+	case int64:
+		return 8, uint64(i)
+	case uint64:
+		return 8, i
+	}
+
+	panic(fmt.Sprintf("not a int type: %v", i))
 }
--- strhelper/strhelper_test.go
+++ strhelper/strhelper_test.go
@@ -3,6 +3,8 @@ package strhelper
 import (
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestToAndFromBitWords(t *testing.T) {
@@ -147,5 +149,35 @@ func TestSliceToAndFromBitWords(t *testing.T) {
 		if !reflect.DeepEqual(c.input, strs) {
 			t.Fatalf("%d-th expect: %v; but: %v", i+1, c.input, strs)
 		}
+	}
+}
+
+func TestIntToBin(t *testing.T) {
+
+	ta := require.New(t)
+
+	cases := []struct {
+		input interface{}
+		want  string
+	}{
+		{int8(7), "11100000"},
+		{uint8(7), "11100000"},
+		{int16(0x0507), "11100000 10100000"},
+		{uint16(0x0507), "11100000 10100000"},
+		{int32(0x01030507), "11100000 10100000 11000000 10000000"},
+		{uint32(0x01030507), "11100000 10100000 11000000 10000000"},
+		{int64(0x0f01030507), "11100000 10100000 11000000 10000000 11110000 00000000 00000000 00000000"},
+		{uint64(0x0f01030507), "11100000 10100000 11000000 10000000 11110000 00000000 00000000 00000000"},
+
+		{[]int8{7}, "11100000"},
+		{[]uint16{0x0507, 0x0102}, "11100000 10100000,01000000 10000000"},
+	}
+
+	for i, c := range cases {
+		got := ToBin(c.input)
+		ta.Equal(c.want, got,
+			"%d-th: input: %#v; want: %#v; got: %#v",
+			i+1, c.input, c.want, got)
+
 	}
 }
