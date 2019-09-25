3012751fd9e322972d3a5ad79edd368a712f3b76
drdr xp <drdr.xp@gmail.com>
2019-09-25 17:08:00 +0800

    new-feature: slimtrie: add GetIxx to optimize integer value

---
+++ trie/slimtrie_getint.go
@@ -0,0 +1,82 @@
+package trie
+
+// GetI8 is same as Get() except it is optimized for int8.
+//
+// Since 0.5.10
+func (st *SlimTrie) GetI8(key string) (int8, bool) {
+
+	eqID := st.GetID(key)
+
+	if eqID == -1 {
+		return 0, false
+	}
+
+	ith, _ := st.getLeafIndex(eqID)
+
+	v := int8(st.nodes.LeafBytes[ith])
+
+	return v, true
+}
+
+// GetI16 is same as Get() except it is optimized for int16.
+//
+// Since 0.5.10
+func (st *SlimTrie) GetI16(key string) (int16, bool) {
+
+	eqID := st.GetID(key)
+
+	if eqID == -1 {
+		return 0, false
+	}
+
+	ith, _ := st.getLeafIndex(eqID)
+	stIdx := ith << 1
+
+	b := st.nodes.LeafBytes[stIdx : stIdx+2]
+
+	v := int16(b[0]) | int16(b[1])<<8
+
+	return v, true
+}
+
+// GetI32 is same as Get() except it is optimized for int32.
+//
+// Since 0.5.10
+func (st *SlimTrie) GetI32(key string) (int32, bool) {
+
+	eqID := st.GetID(key)
+
+	if eqID == -1 {
+		return 0, false
+	}
+
+	ith, _ := st.getLeafIndex(eqID)
+	stIdx := ith << 2
+
+	b := st.nodes.LeafBytes[stIdx : stIdx+4]
+
+	v := int32(b[0]) | int32(b[1])<<8 | int32(b[2])<<16 | int32(b[3])<<24
+
+	return v, true
+}
+
+// GetI64 is same as Get() except it is optimized for int64.
+//
+// Since 0.5.10
+func (st *SlimTrie) GetI64(key string) (int64, bool) {
+
+	eqID := st.GetID(key)
+
+	if eqID == -1 {
+		return 0, false
+	}
+
+	ith, _ := st.getLeafIndex(eqID)
+	stIdx := ith << 3
+
+	b := st.nodes.LeafBytes[stIdx : stIdx+8]
+
+	v := int64(b[0]) | int64(b[1])<<8 | int64(b[2])<<16 | int64(b[3])<<24 | int64(b[4])<<32 | int64(b[5])<<40 | int64(b[6])<<48 | int64(b[7])<<56
+
+	return v, true
+}
---
+++ trie/slimtrie_getint_test.go
@@ -0,0 +1,112 @@
+package trie
+
+import (
+	"testing"
+
+	"github.com/openacid/slim/encode"
+	"github.com/stretchr/testify/require"
+)
+
+func fibhash64(i uint64) uint64 {
+	return i * 11400714819323198485
+}
+
+func TestSlimTrie_GetI8(t *testing.T) {
+
+	ta := require.New(t)
+
+	keys := getKeys("20kvl10")
+	values := make([]int8, len(keys))
+	for i := 0; i < len(keys); i++ {
+		values[i] = int8(fibhash64(uint64(i)))
+	}
+
+	st, err := NewSlimTrie(encode.I8{}, keys, values)
+	ta.NoError(err)
+
+	testUnknownKeysGRS(t, st, randVStrings(len(keys)*5, 0, 10))
+
+	for i, key := range keys {
+
+		dd("test Get: present: %s", key)
+
+		v, found := st.GetI8(key)
+		ta.True(found, "Get:%v", key)
+		ta.Equal(values[i], v, "Get:%v", key)
+	}
+}
+
+func TestSlimTrie_GetI16(t *testing.T) {
+
+	ta := require.New(t)
+
+	keys := getKeys("20kvl10")
+	values := make([]int16, len(keys))
+	for i := 0; i < len(keys); i++ {
+		values[i] = int16(fibhash64(uint64(i)))
+	}
+
+	st, err := NewSlimTrie(encode.I16{}, keys, values)
+	ta.NoError(err)
+
+	testUnknownKeysGRS(t, st, randVStrings(len(keys)*5, 0, 10))
+
+	for i, key := range keys {
+
+		dd("test Get: present: %s", key)
+
+		v, found := st.GetI16(key)
+		ta.True(found, "Get:%v", key)
+		ta.Equal(values[i], v, "Get:%v", key)
+	}
+}
+
+func TestSlimTrie_GetI32(t *testing.T) {
+
+	ta := require.New(t)
+
+	keys := getKeys("20kvl10")
+	values := make([]int32, len(keys))
+	for i := 0; i < len(keys); i++ {
+		values[i] = int32(fibhash64(uint64(i)))
+	}
+
+	st, err := NewSlimTrie(encode.I32{}, keys, values)
+	ta.NoError(err)
+
+	testUnknownKeysGRS(t, st, randVStrings(len(keys)*5, 0, 10))
+
+	for i, key := range keys {
+
+		dd("test Get: present: %s", key)
+
+		v, found := st.GetI32(key)
+		ta.True(found, "Get:%v", key)
+		ta.Equal(values[i], v, "Get:%v", key)
+	}
+}
+
+func TestSlimTrie_GetI64(t *testing.T) {
+
+	ta := require.New(t)
+
+	keys := getKeys("20kvl10")
+	values := make([]int64, len(keys))
+	for i := 0; i < len(keys); i++ {
+		values[i] = int64(fibhash64(uint64(i)))
+	}
+
+	st, err := NewSlimTrie(encode.I64{}, keys, values)
+	ta.NoError(err)
+
+	testUnknownKeysGRS(t, st, randVStrings(len(keys)*5, 0, 10))
+
+	for i, key := range keys {
+
+		dd("test Get: present: %s", key)
+
+		v, found := st.GetI64(key)
+		ta.True(found, "Get:%v", key)
+		ta.Equal(values[i], v, "Get:%v", key)
+	}
+}
