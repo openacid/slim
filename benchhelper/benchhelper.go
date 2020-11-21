f1b175270dae7c154ffba1d14dd9d67802f028ec
drdr xp <drdr.xp@gmail.com>
2020-11-21 23:23:58 +0800

    api-change: benhelper: remove RandI64Slice and RandI32Slice. moved to https://github.com/openacid/testutil

--- benchhelper/benchhelper.go
+++ benchhelper/benchhelper.go
@@ -48,36 +48,6 @@ func RandI32SliceBetween(min int32, max int32, factor float64) []int32 {
 	return rst
 }
 
-func RandI64Slice(min, n, step int64) []int64 {
-	rnd := rand.New(rand.NewSource(time.Now().Unix()))
-
-	rst := make([]int64, 0)
-
-	p := min
-	for i := 0; i < int(n); i++ {
-		s := int64(rnd.Float64() * float64(step))
-		p += s
-		rst = append(rst, p)
-	}
-
-	return rst
-}
-
-func RandI32Slice(min, n, step int32) []int32 {
-	rnd := rand.New(rand.NewSource(time.Now().Unix()))
-
-	rst := make([]int32, 0)
-
-	p := min
-	for i := 0; i < int(n); i++ {
-		s := int32(rnd.Float64() * float64(step))
-		p += s
-		rst = append(rst, p)
-	}
-
-	return rst
-}
-
 func RandSortedStrings(cnt, leng int, from []byte) []string {
 
 	keys := make(map[string]bool, cnt)
